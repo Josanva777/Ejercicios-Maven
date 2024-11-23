c9d8561f9c43853c6ea9fd0a8192b53d4df4a1d3
Josanva777 <johansanchezvalencia@gmail.com>
2024-11-23 18:25:38 -0500

    ejercicio 1 maven

---
+++ demo/src/main/java/com/example/Entidad/Libro.java
@@ -0,0 +1,54 @@
+package com.example.Entidad;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+
+    // Constructor vacío (requerido por JPA)
+    public Libro() {
+    }
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+}
+
--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,64 @@
 package com.example;
 
+import com.example.Entidad.Libro;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("miUnidadPersistencia");
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        crearLibro("El Quijote", "Miguel de Cervantes", 1605);
+        Libro libro = leerLibro(1L);
+        if (libro != null) {
+            System.out.println("Libro encontrado: " + libro.getTitulo());
+        }
+        actualizarLibro(1L, "Don Quijote de la Mancha", "Miguel de Cervantes Saavedra");
+        eliminarLibro(1L);
+        emf.close();
+    }
+
+    public static void crearLibro(String titulo, String autor, int anioPublicacion) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Libro libro = new Libro();
+        libro.setTitulo(titulo);
+        libro.setAutor(autor);
+        libro.setAnioPublicacion(anioPublicacion);
+        em.persist(libro);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public static Libro leerLibro(Long id) {
+        EntityManager em = emf.createEntityManager();
+        Libro libro = em.find(Libro.class, id);
+        em.close();
+        return libro;
+    }
+
+    public static void actualizarLibro(Long id, String nuevoTitulo, String nuevoAutor) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Libro libro = em.find(Libro.class, id);
+        if (libro != null) {
+            libro.setTitulo(nuevoTitulo);
+            libro.setAutor(nuevoAutor);
+            em.merge(libro);
+        }
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public static void eliminarLibro(Long id) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Libro libro = em.find(Libro.class, id);
+        if (libro != null) {
+            em.remove(libro);
+        }
+        em.getTransaction().commit();
+        em.close();
     }
 }
